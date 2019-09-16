a6bc4e8122b7be8c5e61a327b65913ec7f9e6a0c
Clay <Clay@10.0.66.203>
2019-09-15 22:25:44 -0500

    First attempt at committing the code

---
+++ SimpleDiceGame/src/game/GameStart.java
@@ -0,0 +1,12 @@
+package game;
+
+public class GameStart {
+	 
+	   public static void main(String[] args) {
+	      System.out.println("Let's play!");
+	      Game g = new Game();
+	      g.play();
+	   }
+
+	}
+
